c48cd023ff142792bb6f3679b8cf6b3d76c17f0c
rbd3 <rabedesanaandry01@gmail.com>
2024-11-26 19:03:13 +0300

    Optimize fibonacci function

--- Fibonacci.py
+++ Fibonacci.py
@@ -7,3 +7,23 @@ def fibonnacci(number):
      return fibonnacci(number - 1) + fibonnacci(number - 2)
     
 print(fibonnacci(24))
+
+def fiboOptimize(number):
+   a = 0
+   b = 1
+   fibo = 0
+   
+   if number == 0:
+      return 0
+   elif number == 1:
+      return 1
+   
+   for _ in range(2, number + 1):
+      fibo = a + b
+      a = b
+      b = fibo
+   return fibo
+   
+   
+   
+print(fiboOptimize(24))
